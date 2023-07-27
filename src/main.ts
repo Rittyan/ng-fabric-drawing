d2cfe553c1a7b556942bc2e2742b3580a10d3565
Rittyan <ritmo500@gmail.com>
2023-07-27 14:09:03 +0900

    Standalone Componentでアプリを動かすように

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import {AppComponent} from "./app/app.component";
+import {bootstrapApplication} from "@angular/platform-browser";
+import {importProvidersFrom} from "@angular/core";
+import {RouterModule} from "@angular/router";
+import {Route} from "./app/route";
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    // Common Modules
+    importProvidersFrom(RouterModule.forRoot(Route)),
+]}).catch(err => console.error(err));;
