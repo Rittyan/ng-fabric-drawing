455fd2b6a2ae4d2c0ad49bf22521f1b545d2ec37
Rittyan <ritmo500@gmail.com>
2023-07-27 14:20:09 +0900

    Angular Material追加

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import {bootstrapApplication} from "@angular/platform-browser";
 import {importProvidersFrom} from "@angular/core";
 import {RouterModule} from "@angular/router";
 import {Route} from "./app/route";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     // Common Modules
     importProvidersFrom(RouterModule.forRoot(Route)),
+    provideAnimations()
 ]}).catch(err => console.error(err));;
